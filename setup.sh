5b77a23e84bca012661ffdc13ab51d1adaeed7ee
Knut Erik Hollund <3600521+knut-erik@users.noreply.github.com>
2023-07-21 08:55:48 +0200

    Update setup.sh
    Updated to use vim plugins and install them

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
 
+# CP vim resource file
 cp ~/.dotfiles/.vimrc ~/.vimrc
+
+# For vim Plugins
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install VIM Plugins
+vim +'PlugInstall --sync' +qa
